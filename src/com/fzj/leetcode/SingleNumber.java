b7647dc746eed9dc9494f067bf44d83012d46164
fanzj <fanzj_work@163.com>
2017-03-23 19:32:05 +0800

    single-number

---
+++ src/com/fzj/leetcode/SingleNumber.java
@@ -0,0 +1,35 @@
+package com.fzj.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleNumber {
+	
+	//自己的求解
+	public int singleNumber(int[] A){
+		List<Integer> list = new ArrayList<>();
+		for(int i=0;i<A.length;i++){
+			if(!list.contains(A[i])){
+				list.add(A[i]);
+			}	
+			else{
+				list.remove((Integer)A[i]);
+			}	
+		}
+		return list.get(0);
+	}
+	
+	//更好的思路
+	public int singleNumber2(int[] A){
+		int num = A[0];
+		for(int i=1;i<A.length;i++){
+			num ^= A[i];//相同的数异或为0，0与一个数异或还是那个数
+		}
+		return num;		
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {1,4,6,73,5,123,4,1,6,123,5};
+		System.out.println(new SingleNumber().singleNumber2(a));
+	}
+}
