237776db1831e2e2804a2819e77d60df76656a16
fanzj <fanzj_work@163.com>
2017-03-29 14:38:13 +0800

    search-a-2d-matrix

--- src/com/fzj/leetcode/SearchA2dMatrix.java
+++ src/com/fzj/leetcode/SearchA2dMatrix.java
@@ -28,8 +28,8 @@ public class SearchA2dMatrix {
 	
 	public static void main(String[] args) {
 		SearchA2dMatrix solution = new SearchA2dMatrix();
-		//int[][] A = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
-		int[][] A = {{1}};
+		int[][] A = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+		//int[][] A = {{1}};
 		System.out.println(solution.searchMatrix(A, 0));
 	}
 }
