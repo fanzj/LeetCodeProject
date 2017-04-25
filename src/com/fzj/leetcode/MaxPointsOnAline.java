1cffdcc407ed6242acf8b4e8c10fb92437a8d1c1
fanzj <fanzj_work@163.com>
2017-04-25 10:23:08 +0800

    max-points-on-a-line

--- src/com/fzj/leetcode/MaxPointsOnAline.java
+++ src/com/fzj/leetcode/MaxPointsOnAline.java
@@ -40,7 +40,11 @@ public class MaxPointsOnAline {
 						ver++;
 					}
 				}else {
-					double k = (p2.y - p1.y)*1.0/(p2.x-p1.x)*1.0;
+					double k = 0;
+					if(p2.y!=p1.y){
+						k = (double)(p2.y - p1.y)/(p2.x-p1.x);
+					}
+					//System.out.println("k = "+k);
 					if(map.containsKey(k)){
 						map.put(k, map.get(k)+1);
 					}else {
@@ -56,19 +60,67 @@ public class MaxPointsOnAline {
 		}
 		return ret;
 	}
+	
+	public int maxPoints2(Point[] points) {
+		if(points.length<2)
+			return points.length;
+		int ret = 0;
+		for(int i=0;i<points.length-1;i++){
+			int dup = 1;
+			int ver = 0;
+			HashMap<Double, Integer> map = new HashMap<>();
+			Point p1 = points[i];
+			for(int j=i+1;j<points.length;j++){
+				Point p2 = points[j];
+				if(p1.x==p2.x){
+					if(p1.y==p2.y){
+						dup++;
+					}else {
+						ver++;
+					}
+				}else {
+					double k = 0;
+					if(p2.y!=p1.y){
+						k = (double)(p2.y - p1.y)/(p2.x-p1.x);
+					}
+//					System.out.println("k = "+k);
+					if(map.containsKey(k)){
+						map.put(k, map.get(k)+1);
+					}else {
+						map.put(k, 1);
+					}
+				}
+			}
+			
+			int max = ver;
+			for(double k:map.keySet()){
+				max = Math.max(max, map.get(k));
+			}
+//			System.out.println("ret = "+ret+", max = "+max+", dup = "+dup);
+			ret = Math.max(ret, max+dup);
+		}
+		
+		
+		return ret;
+	}
 
 	public static void main(String[] args) {
 		MaxPointsOnAline solution = new MaxPointsOnAline();
-		Point[] points = new Point[8];
+	/*	Point[] points = new Point[8];
 		int[] x = {0,2,3,4,0,0,5,5};
-		int[] y = {0,1,2,0,1,3,0,0};
-	/*	Point[] points = new Point[2];
+		int[] y = {0,1,2,0,1,3,0,0};*/
+		/*Point[] points = new Point[2];
 		int[] x = {0,0};
 		int[] y = {0,0};*/
+		Point[] points = new Point[3];
+		int[] x = {2,3,-5};
+		int[] y = {3,3,3};
 		for(int i=0;i<points.length;i++){
 			Point point = solution.new Point(x[i], y[i]);
 			points[i] = point;
 		}
-		System.out.println(solution.maxPoints(points));
+		System.out.println(solution.maxPoints2(points));
+		double a = 1.0*(0)/(-4);
+		System.out.println(a);
 	}
 }
